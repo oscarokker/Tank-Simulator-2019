c446d256a6f6d8c69b72d737ae3aa3d8de10512c
HIIGARA\oscar <oscarrode99@gmail.com>
2019-03-03 21:54:11 +0100

    Staged the project

---
+++ TankSimulator2019/Source/TankSimulator2019/TankSimulator2019GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankSimulator2019GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSIMULATOR2019_API ATankSimulator2019GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
