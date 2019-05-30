0866da1e17bc103199acc9e9fa6f39c0687a728a
HIIGARA\oscar <oscarrode99@gmail.com>
2019-05-30 12:41:44 +0200

    184 - Getting the rotation of where to aim

--- TankSimulator2019 4.22/Source/TankSimulator2019/Private/TankAimingComponent.cpp
+++ TankSimulator2019 4.22/Source/TankSimulator2019/Private/TankAimingComponent.cpp
@@ -2,7 +2,7 @@
 
 #include "TankAimingComponent.h" // Must be first to include
 #include "TankPlayerController.h"
-#include <Kismet\GameplayStatics.h>
+#include "Kismet\GameplayStatics.h"
 
 
 // Sets default values for this component's properties
@@ -18,48 +18,45 @@ void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
 }
 
 
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-}
-
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-}
-
-
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("It just works"));
-
-	/*
 	if (!Barrel)
 	{
 		return;
 	}
-	*/
 
-	FVector OutLaunchVelocity(0);
+	FVector OutLaunchVelocity;
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
 
-	if (UGameplayStatics::SuggestProjectileVelocity(
-			this,
-			OutLaunchVelocity,
-			StartLocation,
-			HitLocation,
-			LaunchSpeed,
-			false,
-			0,
-			0,
-			ESuggestProjVelocityTraceOption::DoNotTrace
-			)
-		)
+	bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity(
+		this,
+		OutLaunchVelocity,
+		StartLocation,
+		HitLocation,
+		LaunchSpeed,
+		ESuggestProjVelocityTraceOption::DoNotTrace
+	);
+
+	if (bHaveAimSolution)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		UE_LOG(LogTemp, Warning, TEXT("Aiming at %s"), *AimDirection.ToString());
+		MoveBarrelTowards(HitLocation);
 	}
+}
+
+
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
+{
+	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
+	auto AimAsRotator = AimDirection.Rotation();
+	auto DeltaRotator = AimAsRotator - BarrelRotator;
+
+	UE_LOG(LogTemp, Warning, TEXT("AimAsRotator: %s"), AimAsRotator.ToString());
+
+	// Convert float to degrees
+	// Get tower component
+		// Rotate tower component
+	// Get barrel component
+		// rotate barrel component
 }
