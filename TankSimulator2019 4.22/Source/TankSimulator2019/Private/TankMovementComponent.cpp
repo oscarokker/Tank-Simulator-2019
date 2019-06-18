f5156a444e2c2f0e4050b894ee44968aca5998a9
HIIGARA\oscar <oscarrode99@gmail.com>
2019-06-18 11:20:47 +0200

    218 - AI Tanks move toward the player

--- TankSimulator2019 4.22/Source/TankSimulator2019/Private/TankMovementComponent.cpp
+++ TankSimulator2019 4.22/Source/TankSimulator2019/Private/TankMovementComponent.cpp
@@ -16,9 +16,13 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 {
 	// No need to call Super as we're replacing the functionality
 
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to: %s"), *TankName, *MoveVelocityString);
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	
+	IntendMoveForward(ForwardThrow);
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s vectoring to: %s"), *TankName, *MoveVelocityString);
 }
 
 
