35f6230e24e92b3e4154ff011294f976f0af8ebc
HIIGARA\oscar <oscarrode99@gmail.com>
2019-06-23 21:34:00 +0200

    230 - Fixed aiming bug, now tanks can aim

--- TankSimulator2019 4.22/Source/TankSimulator2019/Private/Tank.cpp
+++ TankSimulator2019 4.22/Source/TankSimulator2019/Private/Tank.cpp
@@ -12,9 +12,6 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s NOTICE ME: Tank C++ Construct"), *TankName)
 }
 
 
@@ -22,8 +19,7 @@ void ATank::BeginPlay()
 {
 	Super::BeginPlay(); // Needed for BP to BeginPlay to run
 
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s NOTICE ME: Tank C++ Begin Play"), *TankName)
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
 }
 
 
@@ -37,7 +33,7 @@ void ATank::AimAt(FVector HitLocation)
 
 void ATank::Fire()
 {
-	if (!ensure(Barrel)) {return;}
+	if (!ensure(Barrel)) {return;} // TODO Fix ensure log
 
 	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSecounds;
 	if (isReloaded)
--- TankSimulator2019 4.22/Source/TankSimulator2019/Public/Tank.h
+++ TankSimulator2019 4.22/Source/TankSimulator2019/Public/Tank.h
@@ -36,6 +36,7 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	TSubclassOf<AProjectile> ProjectileBlueprint;
 
+	// TODO Remove once firing is moved to aiming component
 	UPROPERTY(EditDefaultsOnly, Category = "Firing")
 	float LaunchSpeed = 6000;
 
